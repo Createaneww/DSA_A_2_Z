9215a69a7de520992f0f6b39d83ef75a371537e6
Parth Patel <creatinganewway@gmail.com>
2025-08-07 18:13:49 +0530

    updated with optimal approch

--- ARRAYS/HARD/Merging_overlap_range.cpp
+++ ARRAYS/HARD/Merging_overlap_range.cpp
@@ -23,10 +23,31 @@ vector<vector<int>> better_Overlaping_arr(vector<vector<int>>&arr ,int n){
     
 }
 
+
+
+vector<vector<int>> OPtimal_overlapping_arr(vector<vector<int>>&arr , int n){
+    sort(arr.begin() , arr.end());
+    vector<vector<int>>ans;
+    for (int i = 0; i < n; i++)
+    {
+        if(ans.empty() || arr[i][0] > ans.back()[1]){
+            ans.push_back(arr[i]);
+        }
+        else{
+            ans.back()[1] = max(ans.back()[1] , arr[i][1]);
+        }
+    }
+    return ans;
+    
+
+}
+
+
 int main(){
     vector<vector<int>> arr = {{1,3},{2,6},{8,10},{15,18}};
     int n = arr.size();
-    vector<vector<int>> ans = better_Overlaping_arr(arr, n);
+    // vector<vector<int>> ans = better_Overlaping_arr(arr, n);
+    vector<vector<int>> ans = OPtimal_overlapping_arr(arr, n);
 
     cout << "The Final matrix is: "<<endl;
     for(int i = 0 ; i<n ; i++){
