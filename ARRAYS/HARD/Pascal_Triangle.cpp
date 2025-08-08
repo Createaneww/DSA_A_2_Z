02abee88ca241917775820e191a0cca7e5c9aae8
Parth Patel <creatinganewway@gmail.com>
2025-08-08 13:45:52 +0530

    updated Pascal_tri
    type-1 problem

--- ARRAYS/HARD/Pascal_Triangle.cpp
+++ ARRAYS/HARD/Pascal_Triangle.cpp
@@ -1,25 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-int nCr(int n , int r){   //10c3
-    long long res = 1;  
-    //calculating the ele
-    for(int i = 0; i<r ; i++){;
+int NCR(int n , int r){
+    int res = 1;
+    for(int i = 0 ; i<r ; i++){
         res = res*(n-i);
         res = res/(i+1);
     }
     return res;
 }
-int pascalTriangle(int r , int c){  //11c4
-    int ele = nCr( r-1 ,  c-1);
-    return ele;
+
+int Typ1_Pascal_triangle(int r , int c){
+    int res = NCR( r-1 ,  c-1);
+    return res;
 }
+
+
 int main(){
     int r , c;
     cout<<"Enter Row no"<<endl;
     cin>>r;
     cout<<"Enter coloumm no"<<endl;
     cin>>c;
-    int element = pascalTriangle(r , c);
+    // int element = pascalTriangle(r , c);
+    int element = Typ1_Pascal_triangle(r , c);
     cout<<"The value of element on rth row and cth coloumn is :"<<element;
     return 0;
 } 
