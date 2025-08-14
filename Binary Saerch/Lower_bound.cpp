04409ed05762e53d550a2c482e6781e7b3b99d7a
Parth Patel <creatinganewway@gmail.com>
2025-08-14 14:06:34 +0530

    upper bound code ADDED

--- Binary Saerch/Lower_bound.cpp
+++ Binary Saerch/Lower_bound.cpp
@@ -18,10 +18,30 @@ int lowerbound(vector<int>&arr , int x){  // tc = o(logn) base 2;
     
 }
 
+
+int uperbound(vector<int>&arr , int x){
+    int n = arr.size();
+    int low = 0 , high = n-1;
+    int ans = n;
+    while (low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid]>x){
+            ans = mid;
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    
+}
 int main(){
     int x;
     cin>>x;
     vector<int>arr = {1,2,3,3,5,8,8,10,10,11};
-    //lower bound(index of the ele i.e arr[i]>target(x))
-    cout<<lowerbound(arr , x);
+    //lower bound(index of the ele i.e arr[i]>=target(x))
+    // cout<<lowerbound(arr , x);
+
+    //uper bound(index of the ele i.e arr[i]>target(x))
+    cout<<uperbound(arr , x);
 }
