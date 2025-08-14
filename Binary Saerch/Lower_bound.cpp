472e46443dada36ecfa06b997fcfb8060f96e686
Parth Patel <creatinganewway@gmail.com>
2025-08-14 12:27:24 +0530

    lower bound uploded

---
+++ Binary Saerch/Lower_bound.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lowerbound(vector<int>&arr , int x){
+    int n = arr.size();
+    int low = 0 , high = n-1;
+    int ans = n;
+    while (low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid] >= x){   // we get the ans look for left for more smaller;
+            ans = mid;  //storing index in ans variable
+            high = mid-1;
+        }else{
+            low = mid+1;
+        }
+    }
+    return ans;
+    
+}
+
+int main(){
+    int x;
+    cin>>x;
+    vector<int>arr = {1,2,3,3,5,8,8,10,10,11};
+    //lower bound(index of the ele i.e arr[i]>target(x))
+    cout<<lowerbound(arr , x);
+}
