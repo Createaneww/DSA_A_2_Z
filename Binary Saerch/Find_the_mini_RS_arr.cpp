f61137f768cc9c9b36ef0e23cce899f375738c18
Parth Patel <creatinganewway@gmail.com>
2025-08-22 11:07:43 +0530

    updated with min func

--- Binary Saerch/Find_the_mini_RS_arr.cpp
+++ Binary Saerch/Find_the_mini_RS_arr.cpp
@@ -9,6 +9,7 @@ int Mini_element_in_RS_Arr(vector<int>&arr){
     while (low<=high)
     {
         int mid = (low+high)/2;
+        
         //if low is less than high
         if(arr[low]<=arr[high]){
             if(arr[low] <= mini){
@@ -34,7 +35,36 @@ int Mini_element_in_RS_Arr(vector<int>&arr){
     
 }
 
+
+//practice using min func
+int MINI(vector<int>&arr){
+    int mini = INT_MAX;
+    int low = 0 , high = arr.size()-1;
+    while (low<=high)
+    {
+        int mid = (low+high)/2;
+
+        if(arr[low] <= arr[high]){
+            if(arr[low] <= mini ){
+                mini = arr[low];
+            }
+            break;
+        }
+
+        if(arr[low] <= arr[mid]){ // this part is sorted
+            mini = min(mini , arr[low]); //update the mini and and eliminate left
+            low = mid+1;
+        }
+        else{
+            mini = min(mini , arr[mid]);
+            high = mid-1;
+        }
+    }
+    return mini;
+    
+}
 int main(){
     vector<int> arr = {7,8,9,1,2,3,4};
-    cout<< Mini_element_in_RS_Arr(arr);
+    // cout<< Mini_element_in_RS_Arr(arr);
+    cout<< MINI(arr);
 }
