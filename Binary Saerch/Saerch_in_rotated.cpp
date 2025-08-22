55adb39a02463a169cda1f132cb2c699ae77e11b
Parth Patel <creatinganewway@gmail.com>
2025-08-22 10:44:41 +0530

    Updated the search code in rotated sort arr with du[licates

--- Binary Saerch/Saerch_in_rotated.cpp
+++ Binary Saerch/Saerch_in_rotated.cpp
@@ -27,10 +27,43 @@ int search(vector<int>&arr , int target){
 
 }
 
+// search in rotated arr with duplicates
+bool Search(vector<int>&arr , int target){
+   
+    int low = 0 , high = arr.size()-1;
+    while (low<=high)
+    {
+        int mid = (low+high)/2;
+        if(arr[mid] == target) return true;
 
+        if(arr[low] == target && arr[high] == target){
+            low++;
+            high--;
+            continue;
+        }
+
+        if(arr[low] <= arr[mid]){ // left part is sorted
+            if(arr[low] <= target && target <= arr[mid]){ //it is in left part
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }else{
+            if(arr[mid] <= target && target <= arr[high]){ // it is in right part
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        
+    }
+    return false;
+
+}
 int main(){
     int target;
     cin>>target;
     vector<int> arr = {4,5,6,7,0,1,2};
-    cout<< search(arr , target);
+    // cout<< search(arr , target);
+    cout<< Search(arr , target);
 }
