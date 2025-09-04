62c90b8d5a1d9e1a11aa609419d0933d6c25d24b
Parth Patel <creatinganewway@gmail.com>
2025-09-04 16:18:39 +0530

    updated solution optimal

--- Bs on 2D array/Median_matirx.cpp
+++ Bs on 2D array/Median_matirx.cpp
@@ -1,28 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int median(vector<vector<int>> &mat){
-    int low = INT_MAX , high = INT_MIN;
-    int m = mat.size();
-    int n = mat[0].size();
-    for(int i = 0 ; i<n ; i++){
-        low = min(low , mat[i][0]);
-        high = max(high , mat[i][m-1]);
-    }
-    int req = (m*n)/2;
-    while (low<=high)
-    {
-        int mid = (low+high)/2;
-        int smallequal = countSmallEqual(mat ,m , n , mid);
-        if(smallequal <= req){
-            low = mid+1;
-        }else{
-            high = mid-1;
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
         }
     }
-    return low;
-    
+    return ans;
+}
 
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low = INT_MAX, high = INT_MIN;
+
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][n - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(matrix, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
 }
 
 int main() {
@@ -31,8 +53,8 @@ int main() {
         {8, 9, 11, 12, 13},
         {21, 23, 25, 27, 29}
     };
-    
-    int ans = median(matrix);
+    int m = matrix.size(), n = matrix[0].size();
+    int ans = median(matrix, m, n);
     cout << "The median element is: " << ans << endl;
     return 0;
 }
