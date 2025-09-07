2e2059529f7dfcd5baa79b053b03f11a9901c5bd
Parth Patel <creatinganewway@gmail.com>
2025-09-07 21:32:09 +0530

    Create Sum_zero_of_ele.cpp

---
+++ Questions done random/Sum_zero_of_ele.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> sumZero(int n) {
+        vector<int>ans;
+        if(n == 0 || n == 1) return {0};
+        if(n%2 == 1){  //n = odd
+            n = n-1;
+            n = n/2;
+            for(int i = -n ; i<=n ; i++){
+                ans.push_back(i);
+            }
+        }else{  //n = even
+            n = n/2;
+            for(int i = -n ; i<=n ; i++){
+                if(i == 0) continue;
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+    int main(){
+        
+    }
