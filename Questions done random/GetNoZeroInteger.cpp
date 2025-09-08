044e7de15c1d90b04e5f36d90056097e2985636b
Parth Patel <creatinganewway@gmail.com>
2025-09-08 17:27:37 +0530

    Create GetNoZeroInteger.cpp

---
+++ Questions done random/GetNoZeroInteger.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool checkHasZero(int num){
+    while(num>0){
+        if(num % 10 == 0) return false;
+        num = num/10;
+    }
+    return true;
+}
+    vector<int> getNoZeroIntegers(int n) {
+        if(n == 2) return {1,1};
+       for(int a = 1 ; a<n ; a++){
+        int b = n-a;
+        if(checkHasZero(a) && checkHasZero(b)) return {a,b};
+       }
+       return {};
+    }
+
+    int main(){
+        int n;
+        cin>>n;
+        vector<int>result = getNoZeroIntegers(n);
+        for(auto it : result){
+            cout<<it<<" ";
+        }
+    }
