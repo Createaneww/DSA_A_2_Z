65af543e04e6b30cc77c95dc6433df637e1e9698
Parth Patel <creatinganewway@gmail.com>
2025-09-13 23:15:06 +0530

    Create Max_freq_sum_vowel_const.cpp

---
+++ Questions done random/Max_freq_sum_vowel_const.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int maxFreqSum(string s) {
+        int n = s.length();
+        int maxvol = 0 , maxico = 0;
+        int hasharr[256] = {0};
+        for(auto ch : s){
+            hasharr[ch]++;
+        }
+        for(auto it : s){
+            if(it == 'a'|| it == 'e' || it == 'i' || it == 'o' || it == 'u'){
+                maxvol = max(maxvol , hasharr[it]);
+            }else{
+                maxico = max(maxico , hasharr[it]);
+            }
+        }
+    return (maxico + maxvol);
+    }
+
+    int main(){
+        string s = "success";
+        cout<<maxFreqSum(s);
+    }
