77899c91d06174c35bdc18c23b6b3e9055a4465c
Parth Patel <creatinganewway@gmail.com>
2025-09-16 23:01:37 +0530

    Update Count_no_of_SubStrings.cpp

--- Questions done random/Count_no_of_SubStrings.cpp
+++ Questions done random/Count_no_of_SubStrings.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int No_Of_SubStrings(string s){
     int n = s.length();
     int cnt = 0;
-    for(int i = 0 ; i<n ; i++){
+    for(int i = 0 ; i<n ; i++){ //one method to generate and count no of substrings
         for(int j = i ; j<n ;j++){
             cnt++;
         }
