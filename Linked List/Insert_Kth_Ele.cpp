d58c06f9265322659eb7f30197b08c21f51e239e
Parth Patel <creatinganewway@gmail.com>
2025-09-22 13:53:49 +0530

    insertion at tail completed

--- Linked List/Insert_Kth_Ele.cpp
+++ Linked List/Insert_Kth_Ele.cpp
@@ -42,15 +42,33 @@ void printLL(Node* head){
     
 }
 
-Node* InsertkthEl(Node* head , int val){
+//Insert ele in the starting
+Node* InsertHead(Node* head , int val){
     Node* temp = new Node(val , head);
     return temp;
+}
+//Insert Ele in the ending
+Node* InsertTail(Node* head , int val){
+    if(head == NULL){
+        return new Node(val , nullptr);
+    }
+    Node* temp = head;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    //crete a new node
+    Node* newnode = new Node(val , nullptr);
+    temp->next = newnode;
+    return head;
 }
 
 int main(){
  vector<int> arr = {1,2,3,4,10};
  Node*head = convertArrtoLL(arr);
 
-head = InsertkthEl(head , 5);
+// head = InsertHead(head , 5);
+// printLL(head);
+
+head = InsertTail(head , 12);
 printLL(head);
 }
