f1f105304fb952ab5d39238ad7f783784b6d25a8
Parth Patel <creatinganewway@gmail.com>
2025-10-25 16:42:58 +0530

    Update Basic.cpp

--- Linked List/Basic.cpp
+++ Linked List/Basic.cpp
@@ -20,15 +20,23 @@ struct Node
 
 //Arr to LL
 Node* convertArr2LL(vector<int> &arr){
+    // Node* head = new Node(arr[0]);
+    // Node* mover = head;
+    // for (int i = 1; i < arr.size(); i++)
+    // {
+    //     Node* temp = new Node(arr[i]);
+    //     mover->next = temp;
+    //     mover = temp; 
+    // }
+    // return head;
     Node* head = new Node(arr[0]);
     Node* mover = head;
-    for (int i = 1; i < arr.size(); i++)
-    {
+    for(int i = 1 ; i<arr.size() ; i++){
         Node* temp = new Node(arr[i]);
         mover->next = temp;
-        mover = temp; 
+        mover = temp;
     }
-    return head;   
+    return head;
 }
 
 //how to print the length of LL
